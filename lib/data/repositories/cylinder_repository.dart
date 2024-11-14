1226ad191ef91be5e3d908a6cab411532496058a
CodeK254 <codekarma1003@gmail.com>
2024-11-14 13:39:26 +0300

    Mock API Development and Integration

--- lib/data/repositories/cylinder_repository.dart
+++ lib/data/repositories/cylinder_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:gas_sale/data/models/food.dart';
 import 'package:get/get.dart';
 
@@ -6,11 +8,12 @@ class CylinderRepository extends GetConnect {
     List<CylinderModel> cylinderModelList = <CylinderModel>[];
     try{
       final response = await get(
-        "https://1e81016d-be7b-4676-86af-fe9be9c2fe48.mock.pstmn.io/cylinders",
+        "https://6735d1185995834c8a94350f.mockapi.io/api/v1/gas_sale/cylinders",
         headers: {
           'Accept': 'application/json',
         }
       );
+      log(response.body.toString());
       if(response.statusCode == 200){
         List categories = response.body['categories'];
         cylinderModelList = categories.map((category) => CylinderModel.fromJson(category)).toList();
@@ -18,6 +21,7 @@ class CylinderRepository extends GetConnect {
         throw Exception("Failed to retrieve data. ${response.statusCode}");
       }
     } catch(e){
+      log(e.toString());
       rethrow;
     }
 
